cb16fa51712429752a982702e065d56cd4ee1e5d
EithanLlanos <eithanllanosb@gmail.com>
2023-11-01 13:20:32 -0500

    Main

---
+++ 1.1.3.8 LAB - Just numbers - Part 2.cpp
@@ -0,0 +1,25 @@
+//Check the program in the editor.Find all possible compilation errors and logic errors.Fix them.
+//Your version of the program must print the same result as the expected output.
+// Before you use the compiler, try to find the errors only by manual code analysis.
+//
+//#include <stdio.h>
+//
+//////////////////////////////////////////////
+// 
+//int main()
+//{
+//	printf("The value of six is: %d \n", 16 - 10);
+//	return 0;
+//}
+//
+//Expected output
+//The value of six is : 6
+//
+//
+
+#include <stdio.h>
+int main()
+{
+	printf("The value of six is: %d \n", 16 - 10);
+	return 0;
+}
